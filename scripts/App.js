5f9c0afdf87e7879d6f809970ee5126158cff107
najaf2 <najafali@ou.edu>
2021-12-20 23:15:39 -0600

    make tile objects with functionality

--- scripts/App.js
+++ scripts/App.js
@@ -1,9 +1,43 @@
 // Set up board and tiles
 const boardElment = document.getElementById("board")
+const t1 = document.getElementById("t1")
+const t2 = document.getElementById("t2")
+const t3 = document.getElementById("t3")
+const t4 = document.getElementById("t4")
+const t5 = document.getElementById("t5")
+const t6 = document.getElementById("t6")
+const t7 = document.getElementById("t7")
+const t8 = document.getElementById("t8")
+const t9 = document.getElementById("t9")
 
-const tileObj = () => {
+
+// make a list of all dom tiles
+let tileDom = [t1, t2, t3, t4, t5, t6, t7, t8, t9]
+let tileObjArr = []
+// Create a tile Object
+// will let us mark tiles
+const tileObj = (dom) => {
     let marked = false
-    let mark
+    const setMark = (strMark) => {
+        dom.innerHTML = strMark
+    };
 
-    return {marked, mark}
+    return {dom, marked, setMark}
 }
+
+// Loop through and assign 
+// each dom tile to a tile object
+for (let i = 0; i < tileDom.length; i++) {
+    let newObj = tileObj(tileDom[i])
+    tileObjArr.push(newObj)
+} 
+
+// loop through and add an event listener to each
+// tile
+
+for (let i = 0; i < tileObjArr.length; i++) {
+    tileObjArr[i].dom.addEventListener("click", function() {
+        tileObjArr[i].setMark("x")
+    });
+}
+
